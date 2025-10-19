c36a3f3a6392a85cadd7045b08d5bf085c6cbd7b
Dur4cell <a.henao3@utp.edu.co>
2025-10-19 18:08:26 -0500

    actualizacion del codigo de la clinica

--- tercera_actividad/clinica_particular.py
+++ tercera_actividad/clinica_particular.py
@@ -7,4 +7,27 @@ class Persona:
 class Paciente(Persona):
     def __init__(self, nombre, edad, dni, reg_historia_clinica):
         super().__init__(nombre, edad, dni)
-        self.__reg_historia_clinica = reg_historia_clinica        
+        self.__reg_historia_clinica = reg_historia_clinica     
+
+    def quejarse(self):
+        return "¡Me siento mal!"
+    
+class Doctor(Persona):
+    def __init__(self, nombre, edad, dni, especialidad):
+        super().__init__(nombre, edad, dni)
+        self.__especialidad = especialidad
+
+    def diagnosticar(self):
+        return "Después de examinarte, te recetaré un acetaminofén."
+    def crear_historia_clinica(self, paciente, historia):
+        pass
+    
+class historia_clinica:
+    def __init__(self):
+        self.__registros = []
+
+    def agregar_registro(self, registro):
+        self.__registros.append(registro)
+
+    def obtener_registros(self):
+        return self.__registros
